a92290a305e3f266e3c72f84b378fdef014464d2
oybi-alex-grinn <alex.grinn@oybi.com>
2022-11-16 10:54:07 -0500

    add 2

---
+++ real_estate/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': "Estate",
+    'version': '15.0.0.1',
+    'depends': ['base'],
+    'author': "Yo",
+    'category': 'Real Estate/Brokerage',
+    'description': """
+    Description text
+    """,
+    # data files always loaded at installation
+    'data': [
+      
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+
+    ],
+    'dependencies': [
+        'base',
+    ],
+    'license': 'LGPL-3',
+    'application': True,
+}
